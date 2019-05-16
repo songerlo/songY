f709609cd59baa91c08e17b64029d5fd2f0f37f0
songyu <songyu@aiyunxiao.com>
2019-05-16 11:02:22 +0800

    2

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
     port: 8085, // 端口号
     host: 'localhost',
     https: false, // https:{type:Boolean}
-    open: true, //配置自动启动浏览器
+    open: true, // 配置自动启动浏览器
+    publicPath: './'
     // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
   }
 }
