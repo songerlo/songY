73591cc4e67ef6515974a119bfef89621f046f60
songyu <songyu@aiyunxiao.com>
2019-05-20 18:16:04 +0800

    1

--- src/views/canvasChoose.vue
+++ src/views/canvasChoose.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <canvas id="canvas"></canvas>
+        <canvas id="canvas" width="600" height="600"></canvas>
     </div>
 </template>
 
@@ -16,23 +16,47 @@ function can () {
 function lin (ctx, can) {
   ctx.strokeStyle = 'red'
   for (let i = 1; i < can.height / 40; i++) {
-    ctx.moveTo(0, 40 * i)
-    ctx.lineTo(can.height, 40 * i)
+    ctx.moveTo(0 - 0.5, 40 * i - 0.5)
+    ctx.lineTo(can.height - 0.5, 40 * i - 0.5)
   }
-
   ctx.lineWidth = 1
   ctx.stroke()
 }
 // can()
 export default {
   mounted () {
-    can()
+    // can();
+    let canvas = document.getElementById('canvas')
+    let ctx = canvas.getContext('2d')
+    var flag = false
+    let { n, m } = { n: 0, m: 0 }
+    let c = canvas.getBoundingClientRect()
+    canvas.addEventListener('mousedown', function () {
+      flag = true
+      n = event.clientX - c.left * (canvas.width / c.width)
+      m = event.clientY - c.top * (canvas.height / c.height)
+    })
+    canvas.addEventListener('mouseup', function () {
+      flag = false
+    })
+    canvas.addEventListener('mousemove', function () {
+      console.log(n, m)
+      if (flag) {
+        let x = event.clientX - c.left * (canvas.width / c.width)
+        let y = event.clientY - c.top * (canvas.height / c.height)
+        console.log(x, y)
+        ctx.beginPath()
+        ctx.moveTo(n, m)
+        ctx.lineTo(x, y)
+        ctx.stroke()
+      }
+    })
   }
 }
 </script>
 
 <style>
-    canvas {
-        border: solid 1px #333;
-    }
+canvas {
+    border: solid 1px #333;
+}
 </style>
